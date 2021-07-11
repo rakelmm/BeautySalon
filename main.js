526a1adb624e234a9d4c77ee6417d396c0342006
Rakel Moreira <rakelmoreira@Tiagos-MacBook-Pro.local>
2021-07-10 22:22:08 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+/* abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function () {
+        nav.classList.toggle('show')
+    })
+}
+
+/* quando clicar em um item do menu, esconder o menu*/ 
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('show')
+    })
+}
