abdc0c93a3b0d0d7092edddb1d347ec39061f4ec
Rakel Moreira <rakelmoreira@Tiagos-MacBook-Pro.local>
2021-07-12 21:29:27 -0300

    arquivo-js-adicao de sombra no header ao fazer scroll

--- main.js
+++ main.js
@@ -16,3 +16,18 @@ for (const link of links) {
         nav.classList.remove('show')
     })
 }
+
+/* Mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+    // header.classList.add('scroll') 
+    if (window.scrollY >= navHeight) {
+        // scroll é maior que a altura do header
+        header.classList.add('scroll')   
+    } else {
+        // menor que a altura do header
+        header.classList.remove('scroll') 
+    }
+})
